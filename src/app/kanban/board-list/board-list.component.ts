024072c4368ec2f10a7ac6882fe4c71060e45618
serhiirodikov <sergiy1998373@gmail.com>
2020-12-12 14:28:34 +0200

    added board and board list

---
+++ src/app/kanban/board-list/board-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { BoardService } from '../board.service';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Board } from '../board.model';
+
+@Component({
+  selector: 'app-board-list',
+  templateUrl: './board-list.component.html',
+  styleUrls: ['./board-list.component.scss'],
+})
+export class BoardListComponent implements OnInit, OnDestroy {
+  boards: Board[];
+  sub: Subscription;
+
+  constructor(public boardService: BoardService) {}
+
+  ngOnInit(): void {
+    this.sub = this.boardService.getUserBoards().subscribe((boards) => {
+      this.boards = boards;
+    });
+  }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.boards, event.previousIndex, event.currentIndex);
+    this.boardService.sortBoards(this.boards);
+  }
+}
---
+++ src/app/kanban/board/board.component.ts
@@ -0,0 +1,24 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Task } from '../board.model';
+import { BoardService } from '../board.service';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss'],
+})
+export class BoardComponent {
+  @Input() board;
+  taskDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.board.tasks, event.previousIndex, event.currentIndex);
+    this.boardService.updateTasks(this.board.id, this.board.tasks);
+  }
+
+  handleDelete() {
+    this.boardService.deleteBoard(this.board.id);
+  }
+
+  constructor(private boardService: BoardService, private dialog: MatDialog) {}
+}
